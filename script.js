5dc4d57b6a96ef95fb23bcf350d6f06e66505f78
sohel08 <sohelpathan2394@gmail.com>
2022-11-04 13:17:31 +0530

    commit

--- script.js
+++ script.js
@@ -1,34 +1,24 @@
 // select elements
-const hambergur = document.getElementById('hambergur_icon');
+const hambergur = document.getElementById('hamburger');
 const menu = document.getElementById('menu');
 
 // toggle menu
 hambergur.addEventListener('click', () => {
+  hambergur.classList.toggle('active');
   menu.classList.toggle('active');
-
-  // toogle between menu icon and close menu icon
-  if (hambergur.dataset.image == 'open') {
-    hambergur.dataset.image = 'close';
-    hambergur.src = './images/icon-menu-close.svg';
-  } else {
-    hambergur.dataset.image = 'open';
-    hambergur.src = './images/icon-menu.svg';
-  }
 });
 
 // close menu when user click on closeicon, link and outside of navbar
 menu.addEventListener('click', (e) => {
   let target = e.target;
   if (target.classList.contains('link') || target.id === 'menu') {
-    hambergur.src = './images/icon-menu.svg';
-    hambergur.dataset.image = 'open';
-    return menu.classList.remove('active');
+    hambergur.classList.remove('active');
+    menu.classList.remove('active');
   }
 });
 
-// close menu on scrollY
+// close menu on scroll
 window.onscroll = () => {
-  hambergur.src = './images/icon-menu.svg';
-  hambergur.dataset.image = 'open';
   menu.classList.remove('active');
+  hambergur.classList.remove('active');
 };
